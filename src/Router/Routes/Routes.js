2f64aeab88ad2a5da3156e525760cda437c4d195
sohagislampk <sohagislampk@gmail.com>
2022-11-03 13:55:44 +0600

    first commit

---
+++ src/Router/Routes/Routes.js
@@ -0,0 +1,44 @@
+import Main from "../../Layouts/Main";
+import Checkout from "../../Pages/Checkout/Checkout";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import Signup from "../../Pages/Signup/Signup";
+
+
+
+
+
+const { createBrowserRouter } = require("react-router-dom");
+
+const router = createBrowserRouter([
+
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <Signup></Signup>
+            },
+            {
+                path: '/checkout/:id',
+                element: <Checkout></Checkout>,
+                loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
+            }
+        ]
+    }
+
+])
+
+
+
+
+export default router;
